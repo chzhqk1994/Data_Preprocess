7600800a36dd691bceb2e49ef67dc68969f3b88a
unknown <chzhqk1994@gmail.com>
2018-12-07 17:46:24 +0900

    Rename dir name of CompCars dataset

--- remove_duplicate_file_in_two_directory.py
+++ remove_duplicate_file_in_two_directory.py
@@ -7,21 +7,21 @@ original_path = '/home/qisens/Desktop/song/Qt/img001/test/original/'
 filtered_dir = os.listdir(filtered_path)
 original_dir = os.listdir(original_path)
 
-filtered_item=[]
-original_item=[]
+filtered_item = []
+original_item = []
 
 for file in filtered_dir:
     filtered_item.append(file)
 
 for file in original_dir:
     if file[:2] == '._':
-        os.remove(original_path+file)
+        os.remove(original_path + file)
     original_item.append(file)
 
 for i in range(len(filtered_item)):
     for j in range(len(original_item)):
         if filtered_item[i] == original_item[j]:
-            os.remove(original_path+original_item[i])
+            os.remove(original_path + original_item[i])
 
 print(filtered_item)
 print(original_item)
